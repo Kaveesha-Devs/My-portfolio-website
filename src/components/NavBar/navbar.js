8137ce72278250afa5c2df9dd13e3b7634b9cd0f
Kaveesha-Devs <kaveeshasandanuwan@gmail.com>
2025-08-16 18:21:15 +0530

    updated navbar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png"; // Adjust the path as necessary
+import {Link} from "react-scroll"; // Ensure you have react-scroll installed
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className="Logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
